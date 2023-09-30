b6b3ce559b36bbe74d1b44a38c0e440062ea16a4
unknown <shahiniubat99@gmail.com>
2023-10-01 00:14:47 +0600

    added the heroImg

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import heroImg from '../../assets/hero.jpg'
 
 const Home = () => {
   return (
-    <div className="bg-gray-200 ">
-      <div className="sm:max-w-xl md:max-w-full lg:max-w-full items-center   pt-12">
+   <section>
+     <div className="bg-gray-200 ">
+      <div className="sm:max-w-xl md:max-w-full pb-24 lg:pb-52  lg:max-w-full items-center   pt-20">
       <h2 className=" text-center text-2xl leading-tight lg:text-6xl  font-bold text-blue-500">
         Welcome To HeroGadget
       </h2>
@@ -12,7 +14,7 @@ const Home = () => {
         Best E-commerce platform for buying high quality Smart Home <br />{" "}
         Appliances at extremely affordable Price.
       </p>
-      <div className=" text-center flex flex-wrap justify-center gap-5">
+      <div className=" text-center py-5 flex flex-wrap justify-center gap-5">
         <Link to="/shop">
           <button className="shop-btn">Shop Now</button>
         </Link>
@@ -21,7 +23,10 @@ const Home = () => {
         </Link>
       </div>
       </div>
+     
     </div>
+    <img className="w-5/6 mx-auto mb-12 -mt-12 lg:-mt-40 rounded-lg shadow-md bg-gray-500" src={heroImg} alt="" />
+   </section>
   );
 };
 
