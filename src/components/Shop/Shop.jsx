88a2722534c4334e191cd03f36383f44459b37a3
unknown <shahiniubat99@gmail.com>
2023-10-01 11:37:52 +0600

    load the data and product information pass the productCard

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import ProductCard from '../Cards/ProductCard';
 
 const Shop = () => {
+    const productData = useLoaderData();
+    // console.log(productData);
+    
     return (
-        <div>
-            
+      <div className='mx-28  px-4  py-5   md:max-w-full lg:px-8 lg:max-w-screen-xl'>
+          <div className='grid md:grid-cols-2 gap-8 lg:grid-cols-3'>
+            {
+                productData.map(product=><ProductCard
+                key={product.id}
+                product={product}
+                />)
+            }
         </div>
+      </div>
     );
 };
 
